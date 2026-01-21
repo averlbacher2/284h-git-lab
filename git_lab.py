ed8aea46008caeddf5d0e434d77902da23d129ee
averlbacher2 <aerlbacher2@huskers.unl.edu>
2026-01-21 15:17:12 -0600

    made a change

--- git_lab.py
+++ git_lab.py
@@ -61,5 +61,5 @@ def is_prime(n):
     return True
 
 def add_numbers(a, b):
-    """Returns the incorrect sum of two numbers."""
-    return a - b  # Introduces an error
+    """Returns the correct sum of two numbers."""
+    return a + b
